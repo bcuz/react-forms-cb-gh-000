97ac83ee126021818cd9732600d0da9b8ca27257
Adam N <adam.nawroc@gmail.com>
2019-01-03 17:53:10 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
-import DigitalClicker from './components/DigitalClicker';
+import ControlledInput from './components/ControlledInput';
 
 
 ReactDOM.render(
