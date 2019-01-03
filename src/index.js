5022ae01e8e945941a922cef6cb725a3ee51f5a0
Adam N <adam.nawroc@gmail.com>
2019-01-03 17:52:37 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import DigitalClicker from './components/DigitalClicker';
+
+
 ReactDOM.render(
   <div>Mount Components Here</div>,
   document.getElementById('root')
